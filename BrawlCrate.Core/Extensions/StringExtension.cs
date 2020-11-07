f0e8b734a83b4ae7582cca60d04ac5dcdb0b13f3
soopercool101 <soopercoolstages@gmail.com>
2020-11-06 18:15:45 -0800

    Add additional useful StringExtension methods

--- BrawlCrate.Core/Extensions/StringExtension.cs
+++ BrawlCrate.Core/Extensions/StringExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BrawlCrate.Core.Extensions
 {
@@ -7,6 +8,21 @@ namespace BrawlCrate.Core.Extensions
     /// </summary>
     public static class StringExtension
     {
+
+        /// <summary>
+        /// Returns a value indicating whether a specified <see cref="T:System.String"/> occurs within this string. A parameter specifies the type of search to use for the specified string.
+        /// </summary>
+        /// <param name="s">The specified string.</param>
+        /// <param name="value">The string to seek.</param>
+        /// <param name="comparisonType">One of the enumeration values that specifies how the strings will be compared.</param>
+        /// <returns>True if the <paramref name="value">value</paramref> parameter occurs within this string; otherwise, false.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="value">value</paramref> is null.</exception>
+        /// <exception cref="T:System.ArgumentException"><paramref name="comparisonType">comparisonType</paramref> is not a <see cref="T:System.StringComparison"></see> value.</exception>
+        public static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            return s.IndexOf(value, comparisonType) >= 0;
+        }
+
         /// <summary>
         /// Returns a value indicating whether a specified <see cref="T:System.Char"/> occurs within this string.
         /// </summary>
@@ -19,7 +35,7 @@ namespace BrawlCrate.Core.Extensions
         }
 
         /// <summary>
-        /// Returns a value indicating whether a specified <see cref="T:System.Char"/> occurs within this string. A parameter specifies the type of search to use for the specified string.
+        /// Returns a value indicating whether a specified <see cref="T:System.Char"/> occurs within this string. A parameter specifies the type of search to use for the specified char.
         /// </summary>
         /// <param name="s">The specified string.</param>
         /// <param name="value">The char to seek.</param>
@@ -30,6 +46,18 @@ namespace BrawlCrate.Core.Extensions
         public static bool Contains(this string s, char value, StringComparison comparisonType)
         {
             return s.IndexOf(value.ToString(), comparisonType) >= 0;
+        }
+
+        /// <summary>
+        /// Returns the byte length of this string when encoded in UTF-8 format.
+        /// </summary>
+        /// <param name="s">The specified string.</param>
+        /// <returns>The number of bytes produced by encoding the specified string.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="s">s</paramref> is null.</exception>
+        /// <exception cref="T:System.Text.EncoderFallbackException">A fallback occurred (see Character Encoding in the .NET Framework for complete explanation)   -and-  <see cref="P:System.Text.Encoding.EncoderFallback"></see> is set to <see cref="T:System.Text.EncoderExceptionFallback"></see>.</exception>
+        public static int UTF8Length(this string s)
+        {
+            return Encoding.UTF8.GetByteCount(s);
         }
     }
 }
