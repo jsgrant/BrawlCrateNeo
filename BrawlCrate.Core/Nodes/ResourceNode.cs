9f05998a1374d0cabd4ff2081b58c1972988941b
soopercool101 <soopercoolstages@gmail.com>
2020-11-06 18:34:18 -0800

    Add very simple resourcenode framework

--- BrawlCrate.Core/Nodes/ResourceNode.cs
+++ BrawlCrate.Core/Nodes/ResourceNode.cs
@@ -1,10 +1,26 @@
-﻿namespace BrawlCrate.Core.Nodes
+﻿using System;
+
+namespace BrawlCrate.Core.Nodes
 {
     /// <summary>
-    /// The basic common implementation used by nodes in the program.
+    /// The base implementation used by nodes in the program.
     /// </summary>
-    public class ResourceNode
+    public class ResourceNode : IDisposable
     {
+        /// <summary>
+        /// Deconstructor, disposes of the node properly to free up memory.
+        /// </summary>
+        ~ResourceNode()
+        {
+            Dispose();
+        }
 
+        /// <summary>
+        /// Free up memory used by this node.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            // Does nothing yet
+        }
     }
 }
