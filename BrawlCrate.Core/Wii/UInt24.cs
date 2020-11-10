0f378977acf7d4f925346e81d4035a8224a04242
soopercool101 <soopercoolstages@gmail.com>
2020-11-10 12:54:41 -0800

    Structs should be immutable

--- BrawlCrate.Core/Wii/UInt24.cs
+++ BrawlCrate.Core/Wii/UInt24.cs
@@ -10,23 +10,14 @@ namespace BrawlCrate.Core.Wii
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct UInt24 : IEquatable<UInt24>
     {
-        private byte _b0; // 0x0
-        private byte _b1; // 0x1
-        private byte _b2; // 0x2
+        private readonly byte _b0; // 0x0
+        private readonly byte _b1; // 0x1
+        private readonly byte _b2; // 0x2
 
         /// <summary>
         /// The value as represented in a more standard 32-bit unsigned integer format.
         /// </summary>
-        public uint Value
-        {
-            get => _b0 | ((uint) _b1 << 8) | ((uint) _b2 << 16);
-            set
-            {
-                _b0 = (byte)(value & 0xFF);
-                _b1 = (byte)((value >> 8) & 0xFF);
-                _b2 = (byte)((value >> 16) & 0xFF);
-            }
-        }
+        private uint Value => _b0 | ((uint) _b1 << 8) | ((uint) _b2 << 16);
 
         /// <summary>
         /// Constructor from an unsigned 32-bit integer.
@@ -59,6 +50,24 @@ namespace BrawlCrate.Core.Wii
         public static implicit operator BUInt24(UInt24 value)
         {
             return new BUInt24(value._b2, value._b1, value._b0);
+        }
+
+        /// <summary>
+        /// Implicit conversion to <see cref="uint"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="UInt24"/> to convert to the 32-bit equivalent.</param>
+        public static implicit operator uint(UInt24 value)
+        {
+            return value.Value;
+        }
+
+        /// <summary>
+        /// Implicit conversion from <see cref="uint"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="UInt24"/> to convert to the 32-bit equivalent.</param>
+        public static implicit operator UInt24(uint value)
+        {
+            return new UInt24(value);
         }
 
         public bool Equals(UInt24 other)
