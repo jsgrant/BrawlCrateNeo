a1a923215a68496a30313eb42e5863ecedddd70b
soopercool101 <soopercoolstages@gmail.com>
2020-11-17 14:35:39 -0800

    Add MemoryMappedFile.WriteToFile

---
+++ BrawlCrate.Core/Extensions/MemoryMappedFileExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace BrawlCrate.Core.Extensions
+{
+    public static class MemoryMappedFileExtension
+    {
+        public static void WriteToFile(this MemoryMappedFile mem, string path, long size)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path) ??
+                                      throw new InvalidOperationException("Path cannot be null."));
+            using (var file = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                MemoryMappedViewStream m = mem.CreateViewStream(0, size);
+                m.CopyTo(file);
+            }
+        }
+    }
+}
